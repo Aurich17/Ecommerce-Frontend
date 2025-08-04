62db7ab6ab352ab53f1024f730147eeb190c1b3e
Aurich17 <chumpitazismael7@gmail.com>
2025-08-04 11:26:18 -0500

    Header

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [ButtonModule,CardModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
