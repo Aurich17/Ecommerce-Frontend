7ec70e2e73d15c098b2961720c1ae243b2441212
Aurich17 <chumpitazismael7@gmail.com>
2025-08-04 13:50:19 -0500

    Landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,14 +1,60 @@
 import { Component } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import { AccordionModule } from 'primeng/accordion';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [ButtonModule,CardModule],
+  imports: [ButtonModule,CardModule,CarouselModule,AccordionModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
 export class LandingComponent {
 
+  empresas = [
+    {
+      nombre: 'Alexis Gamer',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUyF77H4RFWR1h0IiKmeV3IGySppR1I3zkw&s'
+    },
+    {
+      nombre: 'Compañía de test',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUyF77H4RFWR1h0IiKmeV3IGySppR1I3zkw&s'
+    },
+    {
+      nombre: 'Martin Cavero',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUyF77H4RFWR1h0IiKmeV3IGySppR1I3zkw&s'
+    },
+    {
+      nombre: 'TechBridge Inc.',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUyF77H4RFWR1h0IiKmeV3IGySppR1I3zkw&s'
+    },
+    {
+      nombre: 'London Market',
+      logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnUyF77H4RFWR1h0IiKmeV3IGySppR1I3zkw&s'
+    }
+  ];
+  testimonios = [
+    {
+      nombre: 'Frank Escobedo',
+      mensaje: 'Una plataforma de fácil uso.'
+    },
+    {
+      nombre: 'Juan Esteban',
+      mensaje: 'Productos buenos y económicos.'
+    },
+    {
+      nombre: 'Juliana Pérez',
+      mensaje: 'Pude comprar un excelente producto.'
+    },
+    {
+      nombre: 'Carla Mendoza',
+      mensaje: 'El sistema de pagos en cuotas me ayudó muchísimo.'
+    },
+    {
+      nombre: 'Pedro Álvarez',
+      mensaje: 'Rápido, confiable y muy intuitivo.'
+    }
+  ];
 }
