e65a1e0df8df5840c025e65761cfb7a84f88d542
Aurich17 <chumpitazismael7@gmail.com>
2025-08-07 23:21:42 -0500

    Cambios

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,33 +1,12 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { NgClass } from '@angular/common';
-import { FormsModule } from '@angular/forms'; // ✅ IMPORTANTE
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [
-    RadioButtonModule,
-    ButtonModule,
-    RouterModule,
-    NgClass,
-    FormsModule // ✅ Esto habilita [(ngModel)]
-  ],
+  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  selectedType: string = '';
 
-  constructor(private router: Router) {}
-
-  continuar() {
-    if (this.selectedType === 'cliente') {
-      this.router.navigate(['/register/cliente']);
-    } else if (this.selectedType === 'tienda') {
-      this.router.navigate(['/register/tienda']);
-    }
-  }
 }
