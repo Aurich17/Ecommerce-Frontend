87253ebbfea687c1ecaf81ac16dfa11be1a16634
Aurich17 <chumpitazismael7@gmail.com>
2025-08-07 23:20:28 -0500

    CAMBIOS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,21 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LandingComponent } from './landing/landing.component';
+import { EmpresaComponent } from './auth/register/empresa/empresa.component';
+import { ClienteComponent } from './auth/register/cliente/cliente.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
   {
     path: '',
     component: LandingComponent
-  }
+  },
+  {
+  path: 'register',
+  children: [
+    { path: '', component: RegisterComponent }, // selección
+    { path: 'cliente', component: ClienteComponent },
+    { path: 'empresa', component: EmpresaComponent }
+  ]
+}
 ];
