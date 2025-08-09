ea2444980a3238282136d834c2c15783f3a122e0
Zhong76 <zhonglix76@gmail.com>
2025-08-09 15:14:02 -0500

    efwert

--- src/app/admin/landing/pie-pagina/pie-pagina.component.ts
+++ src/app/admin/landing/pie-pagina/pie-pagina.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AccordionModule } from 'primeng/accordion';
+import { CommonModule } from '@angular/common';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-pie-pagina',
   standalone: true,
-  imports: [],
+  imports: [AccordionModule, CommonModule, ReactiveFormsModule, InputTextModule, ButtonModule],
   templateUrl: './pie-pagina.component.html',
   styleUrl: './pie-pagina.component.css'
 })
 export class PiePaginaComponent {
+  piepaginaform = new FormGroup({
+    correo: new FormControl(null),
+    telefono: new FormControl(null),
+    titulo: new FormControl(null),
+    descripcion: new FormControl(null),
+    descripcionizq: new FormControl(null),
+    copyright: new FormControl(null)
+  });
 
+  guardarCambios() {
+  }
 }
