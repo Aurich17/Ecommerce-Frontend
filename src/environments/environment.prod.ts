48e27a5489827849d18056c2e18a04ae679afc84
Aurich17 <chumpitazismael7@gmail.com>
2025-08-29 22:10:35 -0500

    CAMBIOS

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,8 @@ export const environment = {
   production: true,
   imagekitPublicKey: 'public_DxkBzddQ+xXaWR5G0Aj6CoOsaFA=',
   imagekitUrlEndpoint: 'https://ik.imagekit.io/crpweacyf',
-  urlApi: 'https://ecommerce-backend-na5u.onrender.com'
+  urlApi: 'https://ecommerce-backend-na5u.onrender.com',
+  supabaseUrl: 'https://llfjghtbwpbcdtqzxacl.supabase.co',
+  supabaseKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxsZmpnaHRid3BiY2R0cXp4YWNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ4ODM2NjMsImV4cCI6MjA3MDQ1OTY2M30.Fieos8sTOs17AWHgvb2dW0cOLj5TL0zLszBrOjkZd2M',
 };
